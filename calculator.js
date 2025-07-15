b471b95c4ac4ee4c3a1ab1dadad0d6b972496849
dprings7 <davepringle07@gmail.com>
2025-07-15 21:59:19 +0100

    Add basic CSS layout

---
+++ calculator.js
@@ -0,0 +1,38 @@
+function add (a, b) {
+    return a + b;
+}
+
+function subtract (a, b) {
+    return a - b;
+}
+
+function multiply (a, b) {
+    return a * b;
+}
+
+function divide (a, b) {
+    return a / b;
+}
+
+function operate(operator, first, second) {
+    let result = '';
+    switch (operator) {
+        case '+':
+            result = add(first, second);
+            break;
+        case '-':
+            result = subtract(first, second);
+            break;
+        case '*':
+            result = multiply(first, second);
+            break;
+        case '/':
+            result = divide(first, second);
+            break;
+    }
+    return result;
+}
+
+let currentOperator = '';
+let firstOperand = '';
+let secondOperand = '';
